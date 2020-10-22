474d5835a933dbba8ca64f330e6637cfd7e24da3
blubkeman <46632066+blubkeman@users.noreply.github.com>
2020-10-22 14:01:00 -0500

    Update Globals.h

--- Globals.h
+++ Globals.h
@@ -1,6 +1,8 @@
-/*
- * Globals.h - Global variables for the B.L.A.C.Box system
- * Created by Brian Lubkeman, 21 May 2020
+/* =================================================================================
+ *    B.L.A.C.Box: Brian Lubkeman's Astromech Controller
+ * =================================================================================
+ * Globals.h - Library for global definitions
+ * Created by Brian Lubkeman, 22 October 2020
  * Inspired by S.H.A.D.O.W. controller code written by KnightShade
  * Released into the public domain.
  */
@@ -11,35 +13,10 @@
 #include "Settings.h"
 #include "Buffer.h"
 
-typedef struct {
-    //put your variable definitions here for the data you want to send
-    //THIS MUST BE EXACTLY THE SAME ON THE OTHER ARDUINO
-    int hpx; // hp movement
-    int hpy; // hp movement
-    int hpl; // hp light
-    int hpa; // hp automation
-    int dsp; // 100=no change, 0=random, 1=alarm, 4=whistle, 5=leia, 6=short circut, 10=EQ, 11=alarm2,
-}  SEND_DATA_STRUCTURE;
-// 21=speed1, 22=speed2, 23=speed3, 24=logics+, 25=logics-
+const uint8_t PRIMARY = 0;
+const uint8_t SECONDARY = 1;
 
-enum PanelSetting {
-  usePanel = 0,
-  startDelay = 1,
-  openTime = 2,
-  panelStatus = 3,
-  startTime = 4
-};
-
-enum PanelStatus {
-  statusClosed = 0,
-  statusOpen = 1,
-  statusClose = 2
-};
-
-static int cmd;    // We use this to give directions to peripherals based on controller input.
-static bool runningCustomRoutine = false;
-
-// The following variables are used for debugging.
-String output = "";
+const int domeInterruptPin = 2;
+const int footInterruptPin = 3;
 
 #endif
